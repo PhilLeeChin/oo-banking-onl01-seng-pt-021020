a628d1d173719ea3ec773010db6f8b40468f8d5a
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-09 17:12:15 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,26 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
 
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(depo_amo)
+    self.balance += depo_amo
+  end
+
+  def display_balance
+    "Your balance is $#{balance}."
+  end
+
+  def valid?
+    balance > 0 && status == "open" ? true : false
+  end
+
+  def close_account
+    self.status = "closed"
+  end
 end
