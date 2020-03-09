f3249ae3b2373e87317ef0a7e7950e9062bedf8d
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-09 16:34:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,12 +1,4 @@
 class Transfer
   # your code here
-  attr_accessor :transfer, :status
-  attr_reader :amount, :sender, :receiver,
 
-  def initialize(sender)
-    @sender = sender
-    @receiver = receive
-    @status = "pending"
-    @amount = amount
-  end
 end
