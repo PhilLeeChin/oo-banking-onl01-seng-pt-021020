a9910ab5a8c3def72a997145838585dcbe7fdac4
Philip Lee-Chin <phil.leechin@outlook.com>
2020-03-09 17:00:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
-    else
+    elsif valid? && sender.balance < amount
       self.status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
